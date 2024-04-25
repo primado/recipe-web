0cab9dee3d468bdcf16c9c9af344eac741281d79
primado <primadoprincepa@gmail.com>
2024-04-25 18:55:14 +0000

    working on file upload

---
+++ app/profile/page.tsx
@@ -0,0 +1,19 @@
+import Profile from "@/components/detail/settings";
+import Navbar from "@/components/universal/navbar";
+
+
+
+
+
+export default function UpdateProfile() {
+
+    return (
+        <>
+            <Navbar />
+            <section className="my-10 w-full bg-white">
+                <Profile />
+            </section>
+        </>
+    )
+    
+};
